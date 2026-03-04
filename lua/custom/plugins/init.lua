8cfe1090fa2ea33ee827700cc6c7488cb5ef7c49
plasmaDestroyer <satyasheel2004@gmail.com>
2026-03-04 05:46:35 +0530

    Initial commit: Kickstart + LazyVim hybrid setup

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,27 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  -- Configure LazyVim to load tokyonight
+  -- {
+  --   'LazyVim/LazyVim',
+  --   opts = {
+  --     colorscheme = 'tokyonight',
+  --   },
+  -- }, -- Optionally customize tokyonight
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   opts = {
+  --     style = 'moon', -- Options: storm, moon, night, day
+  --     transparent = true, -- Optional: enable transparency
+  --   },
+  -- },
+  -- {
+  --   'ThePrimeagen/vim-be-good',
+  --   opts = {},
+  -- },
+}
